648f164bb015b269db28e680b16a757ba679c018
Alexandre BERREBI <alexandre.berrebi@businessagile.eu>
2017-07-14 23:22:17 +0200

    Create tests for Index views

--- polls/tests.py
+++ polls/tests.py
@@ -1,6 +1,7 @@
 import datetime
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 from .models import Question
 
 # Create your tests here.
@@ -13,7 +14,7 @@ class QuestionModelTest(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
- 
+
     def test_was_published_recently_with_old_question(self):
         """
         was_published_recently() returns False for questions whose pub_date is older than 1 day
@@ -29,3 +30,37 @@ class QuestionModelTest(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionIndexViewsTest(TestCase):
+    def test_no_question(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertIs(response.status_code, 200)
+        self.assertContains(response, "No polls are availables")
+        self.assertQuerysetEqual(response.context['latest_questions_list'], [])
+
+    def test_past_question(self):
+        past_question = create_question(question_text='Past question', days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_questions_list'], ['<Question: Past question>'])
+
+    def test_future_question(self):
+        future_question = create_question(question_text='Future question', days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_questions_list'], [])
+
+    def test_past_and_future_questions(self):
+        past_question = create_question(question_text='Past question', days=-30)
+        future_question = create_question(question_text='Future question', days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_questions_list'], ['<Question: Past question>'])
+
+    def test_2_past_questi9ns(self):
+        past_question_1 = create_question(question_text='Past question 1', days=-5)
+        past_question_2 = create_question(question_text='Past question 2', days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_questions_list'], ['<Question: Past question 1>', '<Question: Past question 2>'])
